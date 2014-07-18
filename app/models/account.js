a6d9962ba421bec0822bf8dc42d3545a83305c61
Kayla Jones <Kaylalyne424@gmail.com>
2014-07-18 14:18:52 -0500

    completed bank and account models + testing

--- app/models/account.js
+++ app/models/account.js
@@ -7,14 +7,35 @@ function Account(acctNum, name, deposit, type){
   this.type = type;
   this.deposits = [];
   this.withdraws = [];
-
+  this.isActive = true;
+  this.overdrafts = 0;
 }
 
 Account.prototype.deposit = function(amount){
-  amount = parseFloat(amount);
-  this.balance += amount;
-  this.deposits.push(amount);
-
+  if(this.isActive){
+    amount = parseFloat(amount);
+    this.balance += amount;
+    this.deposits.push(amount);
+  }
 };
+
+Account.prototype.withdraw = function(amount){
+  if(this.isActive && this.overdrafts <= 3){
+    amount = parseFloat(amount);
+    if(this.balance < amount){
+      amount += 50;
+      this.overdrafts += 1;
+    }
+
+    this.balance -= amount;
+    this.withdraws.push(amount);
+
+    if(this.overdrafts >= 3){
+      this.isActive = false;
+    }
+  }
+};
+
+
 
 module.exports = Account;
--- test/unit/account.js
+++ test/unit/account.js
@@ -17,6 +17,8 @@ describe('Account', function(){
       expect(a1.type).to.equal('savings');
       expect(a1.deposits).to.have.length(0);
       expect(a1.withdraws).to.have.length(0);
+      expect(a1.isActive).to.be.true;
+      expect(a1.overdrafts).to.equal(0);
     });
   });
   describe('#deposit', function(){
@@ -26,8 +28,40 @@ describe('Account', function(){
       expect(a1.balance).to.equal(2000);
       expect(a1.deposits).to.have.length(1);
     });
+    it('should not be able to deposit to suspended account', function(){
+      var a1 = new Account(3, 'Sara', 1500, 'savings');
+      a1.isActive = false;
+      a1.deposit(500);
+      expect(a1.balance).to.equal(1500);
+      expect(a1.deposits).to.have.length(0);
+    });
   });
+  describe('#withdraw', function(){
+    it('should decrease account balance', function(){
+      var a1 = new Account(3, 'Sara', 1500, 'savings');
+      a1.withdraw(500);
+      expect(a1.balance).to.equal(1000);
+      expect(a1.withdraws).to.have.length(1);
+    });
+    it('should increase overdrafts & charge fee when overdrawn', function(){
+      var a1 = new Account(3, 'Sara', 0, 'savings');
+      a1.withdraw(500);
+      expect(a1.overdrafts).to.equal(1);
+      expect(a1.balance).to.equal(-550);
+    });
+    it('should suspend account after 3 overdrafts', function(){
+      var a1 = new Account(3, 'Sara', 0, 'savings');
+      for(var i = 0; i <= 4; i++){
+        a1.withdraw(500);
+      }
+      expect(a1.overdrafts).to.equal(3);
+      expect(a1.balance).to.equal(-1650);
+      expect(a1.withdraws).to.have.length(3);
+      expect(a1.isActive).to.be.false;
+    });
 
+
+  });
 
 });
 
